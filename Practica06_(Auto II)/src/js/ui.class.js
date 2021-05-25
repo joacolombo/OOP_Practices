01a0eca30151399cc0a0748b44aea85fadce38d5
Joaquin Colombo <joacolombo@outlook.com>
2021-05-24 21:48:23 -0300

    Ejercicio del auto

--- Practica06_(Auto II)/src/js/ui.class.js
+++ Practica06_(Auto II)/src/js/ui.class.js
@@ -1,6 +1,8 @@
 const coche  = new Coche();
 let auto = document.getElementById("auto");
 
+auto.innerHTML = `<img src="/src/img/coche.png" width=100px>`; 
+
 function move(event){
 
     if(event.keyCode == "39"){
@@ -8,16 +10,12 @@ function move(event){
         coche.moveRight();
         auto.innerHTML = `<img src="/src/img/coche.png" width=100px>`; 
         auto.style.left = coche.ejeX + "%";
-        
-        
 
     }else if(event.keyCode == "37"){
 
         coche.moveLeft();
         auto.innerHTML = `<img src="/src/img/cocheLeft.png" width=100px>`; 
         auto.style.left = coche.ejeX + "%";
-
-        
 
     }else if(event.keyCode == "38"){
 
@@ -30,7 +28,6 @@ function move(event){
         coche.moveDown();
         auto.innerHTML = `<img src="/src/img/cocheDown.png" width=100px>`; 
         auto.style.top = (-coche.ejeY) + "%";
-        
 
     }
 }
